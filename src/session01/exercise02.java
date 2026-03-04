e7846899002296a83bfbb84b64e2dac320061c6f
jenny-min <jennynguyen.92.92@gmail.com>
2026-03-04 21:06:45 +0900

    create exercise02.java

---
+++ src/session01/exercise02.java
@@ -0,0 +1,13 @@
+package session01;
+
+public class exercise02 {
+    public static void main(String[] args) {
+        //Khai báo biến
+        String name = "Nguyễn Thị Thanh Thùy";
+        int age = 34;
+        String address = "Đồng Nai";
+
+        //In thông tin
+        System.out.printf("Tên tôi là %s, tôi %d tuổi, hiện tại đang học tại PTIT-HCM. Quê ở %s\n", name, age, address);
+    }
+}

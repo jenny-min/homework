4a40dbf2884df267b1dcda8566940c05553e2344
jenny-min <jennynguyen.92.92@gmail.com>
2026-03-04 21:22:57 +0900

    create exercise03.java

---
+++ src/session01/exercise03.java
@@ -0,0 +1,27 @@
+package session01;
+
+import java.util.Scanner;
+
+public class exercise03 {
+    public static void main(String[] args) {
+        //Khai báo biến
+        double radius;//bán kinh
+        double area;//diện tích
+
+        //Tạo đối tượng để nhập
+        Scanner scanner = new Scanner(System.in);
+
+        //Yêu cầu nhập bán kính
+        System.out.printf("Vui lòng nhập bán kinh: ");
+        radius = scanner.nextDouble();
+
+        //Tính diện tích hình tròn
+        area = Math.PI * radius * radius;
+
+        //Hiển thị kết quả
+        System.out.printf("Diện tích hình tròn có bán kính %.2f là: %.2f%n", radius, area);
+
+        //Đóng Scanner
+        scanner.close();
+    }
+}

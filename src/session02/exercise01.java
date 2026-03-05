3fa6c6039459496450eddacf08e968b1d2279b34
jenny-min <jennynguyen.92.92@gmail.com>
2026-03-05 13:33:39 +0900

    create session02/exercise01.java

---
+++ src/session02/exercise01.java
@@ -0,0 +1,26 @@
+package session02;
+
+import java.util.Scanner;
+
+public class exercise01 {
+    public static void main(String[] args){
+        //Tạo đối tượng để người dùng nhập vào giá trị
+        Scanner sc = new Scanner(System.in);
+
+        //yêu cầu người dùng nhập vào số nguyên
+        System.out.printf("Vui lòng nhập vào 1 số nguyên: ");
+        int n = sc.nextInt();
+
+        //Kiểm tra có phải số chẵn hay không
+        if (n == 0) {
+            System.out.printf("Số không phải chẵn cũng không phải lẻ");
+        } else if (n%2 == 0) {
+            System.out.printf("Số %d là số chẵn", n);
+        } else {
+            System.out.printf("Số %d là số lẻ", n);
+        }
+
+        //Đóng scanner
+        sc.close();
+    }
+}
